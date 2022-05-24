72441f9ff1c08c3891176d27717438093e065534
Teza1 <64024742+Teza1@users.noreply.github.com>
2022-05-24 13:42:02 +0100

    fixing security issues updated liberies

---
+++ robofriend/src/components/Card.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-template-curly-in-string */
+import React from "react";
+
+const Card = ({ name, email, id }) => {
+    return (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='robot' src={'https://robohash.org/${id}'} />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
